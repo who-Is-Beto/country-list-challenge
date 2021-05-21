b4f5ee08c9ff7f7bddc4bc92c97119f324810511
whoIsBeto <roberto@tinkin.one>
2021-05-21 00:36:59 -0500

    [add] styles to country card

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import CardCountry from './CardCountry'
+import '../styles/CountryList.css'
 
 function CountryList() {
   return (
-    <>
+    <div className="country-list-container">
       <CardCountry />
-    </>
+    </div>
   )
 }
 
